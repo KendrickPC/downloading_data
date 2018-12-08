43752a98ed6880a5ecbeb86ccf7710e0b68e1859
KendrickPC <kennethpchang@gmail.com>
2018-12-08 14:39:11 +0800

    getting high temperatures from the data.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Download Data from the following site:
 
 https://www.wunderground.com/history/
 
+Parsing the CSV File Headers
+
+<!-- Run high_lows.py in Python -->
+
---
+++ high_lows.py
@@ -0,0 +1,16 @@
+import csv
+
+# Get high temperatures from file.
+filename = 'sitka_weather_07-2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    highs = []
+    for row in reader:
+        highs.append(row[1])
+
+    print(highs)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
