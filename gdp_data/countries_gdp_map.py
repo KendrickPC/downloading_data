64df74d93c5e25d9d8831d2ccc459cff16880f24
KendrickPC <kennethpchang@gmail.com>
2018-12-18 17:46:41 +0800

    GDP python map complete.

--- gdp_data/countries_gdp_map.py
+++ gdp_data/countries_gdp_map.py
@@ -12,6 +12,7 @@ import json
 
 from pygal.maps.world import World
 from countries_gdp import get_country_code
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
 # Load the data into a list.
 filename = 'gdp.json'
@@ -21,14 +22,31 @@ with open(filename) as f:
 
 # Print the GDP for each country.
 # Building a dictionary for GDP data.
+cc_gdp = {}
 for gdp_dict in gdp_data:
     if gdp_dict['Year'] == 2016:
         country_name = gdp_dict['Country Name']
         gdp_value = int(float(gdp_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(code + ": " + str(gdp_value))
-        else:
-            print('ERROR - ' + country_name)
+            cc_gdp[code] = gdp_value
+        # Prints Error value keypairs
+        #     print(code + ": " + str(gdp_value))
+        # else:
+        #     print('ERROR - ' + country_name)
 
         # print(country_name + ": " + str(gdp_value))
+
+wm_style = RS('#336699', base_style=LCS)
+wm = World(style=wm_style)
+wm.force_uri_protocol = 'http'
+wm.title = 'Gross Domestic Product in 2016, by Country'
+wm.add('2016', cc_gdp)
+
+wm.render_to_file('gross_domestic_product.svg')
+
+
+
+
+
+
